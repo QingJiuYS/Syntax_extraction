6ceb0265a37ed40331586c53239095f71ef1ad3d
QingJ <80376515+QingJiuYS@users.noreply.github.com>
2024-03-06 14:31:41 +0800

    Create packet-dccp.c

---
+++ main_parsing/truth_fields/packet-dccp.c
@@ -0,0 +1,175 @@
+[
+    {
+        "field_vari_name": "dccph->sport",
+        "field_offset": "offset",
+        "field_length": 2,
+        "field_endian": "ENC_BIG_ENDIAN"
+    },
+    {
+        "field_vari_name": "dccph->dport",
+        "field_offset": "offset + 2",
+        "field_length": 2,
+        "field_endian": "ENC_BIG_ENDIAN"
+    },
+    {
+        "field_vari_name": "dccph->data_offset",
+        "field_offset": "offset",
+        "field_length": 1,
+        "field_endian": "ENC_NA"
+    },
+    {
+        "field_vari_name": "dccph->cscov",
+        "field_offset": "offset",
+        "field_length": 1,
+        "field_bit_length": "0x0F",
+        "field_endian": "ENC_NA"
+    },
+    {
+        "field_vari_name": "dccph->ccval",
+        "field_offset": "offset",
+        "field_length": 1,
+        "field_bit_length": "0xF0",
+        "field_endian": "ENC_NA"
+    },
+    {
+        "field_vari_name": "dccph->checksum",
+        "field_offset": "offset",
+        "field_length": 2,
+        "field_endian": "ENC_BIG_ENDIAN"
+    },
+    {
+        "field_vari_name": "dccph->reserved1",
+        "field_offset": "offset",
+        "field_length": 1,
+        "field_bit_length": "0xE0",
+        "field_endian": "ENC_NA"
+    },
+    {
+        "field_vari_name": "dccph->type",
+        "field_offset": "offset",
+        "field_length": 1,
+        "field_bit_length": "0x1E",
+        "field_endian": "ENC_NA"
+    },
+    {
+        "field_vari_name": "dccph->x",
+        "field_offset": "offset",
+        "field_length": 1,
+        "field_bit_length": "0x01",
+        "field_endian": "ENC_NA"
+    },
+    {
+        "field_vari_name": "dccph->reserved2",
+        "field_offset": "offset",
+        "field_length": 1,
+        "field_endian": "ENC_NA"
+    },
+    {
+        "field_vari_name": "dccph->seq",
+        "field_offset": "offset",
+        "field_length": 6,
+        "field_endian": "ENC_BIG_ENDIAN"
+    },
+    {
+        "field_vari_name": "dccph->seq",
+        "field_offset": "offset",
+        "field_length": 3,
+        "field_endian": "ENC_BIG_ENDIAN"
+    },
+    {
+        "field_vari_name": "dccph->service_code",
+        "field_offset": "offset",
+        "field_length": 4,
+        "field_endian": "ENC_BIG_ENDIAN"
+    },
+    {
+        "field_vari_name": "dccph->ack_reserved",
+        "field_offset": "offset",
+        "field_length": 2,
+        "field_endian": "ENC_BIG_ENDIAN"
+    },
+    {
+        "field_vari_name": "dccph->ack",
+        "field_offset": "offset + 2",
+        "field_length": 2,
+        "field_endian": "ENC_BIG_ENDIAN"
+    },
+    {
+        "field_vari_name": "dccph->service_code",
+        "field_offset": "offset",
+        "field_length": 4,
+        "field_endian": "ENC_BIG_ENDIAN"
+    },
+    {
+        "field_vari_name": "dccph->ack_reserved",
+        "field_offset": "offset",
+        "field_length": 2,
+        "field_endian": "ENC_BIG_ENDIAN"
+    },
+    {
+        "field_vari_name": "dccph->ack",
+        "field_offset": "offset + 2",
+        "field_length": 2,
+        "field_endian": "ENC_BIG_ENDIAN"
+    },
+    {
+        "field_vari_name": "dccph->ack_reserved",
+        "field_offset": "offset",
+        "field_length": 1,
+        "field_endian": "ENC_NA"
+    },
+    {
+        "field_vari_name": "dccph->ack",
+        "field_offset": "offset + 1",
+        "field_length": 1,
+        "field_endian": "ENC_NA"
+    },
+    {
+        "field_vari_name": "dccph->ack_reserved",
+        "field_offset": "offset",
+        "field_length": 2,
+        "field_endian": "ENC_BIG_ENDIAN"
+    },
+    {
+        "field_vari_name": "dccph->ack",
+        "field_offset": "offset + 2",
+        "field_length": 2,
+        "field_endian": "ENC_BIG_ENDIAN"
+    },
+    {
+        "field_vari_name": "dccph->reset_code",
+        "field_offset": "offset",
+        "field_length": 1,
+        "field_endian": "ENC_NA"
+    },
+    {
+        "field_vari_name": "dccph->data1",
+        "field_offset": "offset + 1",
+        "field_length": 1,
+        "field_endian": "ENC_NA"
+    },
+    {
+        "field_vari_name": "dccph->data2",
+        "field_offset": "offset + 2",
+        "field_length": 1,
+        "field_endian": "ENC_NA"
+    },
+    {
+        "field_vari_name": "dccph->data3",
+        "field_offset": "offset + 3",
+        "field_length": 1,
+        "field_endian": "ENC_NA"
+    },
+    {
+        "field_vari_name": "dccph->ack_reserved",
+        "field_offset": "offset",
+        "field_length": 2,
+        "field_endian": "ENC_BIG_ENDIAN"
+    },
+    {
+        "field_vari_name": "dccph->ack",
+        "field_offset": "offset + 2",
+        "field_length": 2,
+        "field_endian": "ENC_BIG_ENDIAN"
+    }
+]
