12eda667378c52f238fbae17c51016ddd6fa6fa0
QingJ <80376515+QingJiuYS@users.noreply.github.com>
2024-03-09 18:00:32 +0800

    Update select_protocol.py

--- select_protocol.py
+++ select_protocol.py
@@ -2,21 +2,25 @@ import json
 import os
 import csv
 
-
-def convert_to_map(proto_list, dissect_list):
+# Combine protocols and dissectors
+def convert_to_map(proto_table, dissect_table, map_file_path):
     proto_dissect_map = []
-    for proto in proto_list:
+    for proto in proto_table:
         current_map = {}
         proto_name = proto['proto_name']
         short_name = proto['short_name']
         current_map['proto_name'] = proto_name
         current_map['short_name'] = short_name
         current_map['dissect_name'] = []
-        for dissect in dissect_list:
+        for dissect in dissect_table:
             if dissect['dissect_name'] and dissect['proto_name'] == proto_name:
                 dissect_name = dissect['dissect_name']
                 current_map['dissect_name'].append(dissect_name)
         proto_dissect_map.append(current_map)
+
+
+    write_to_map(map_file_path, proto_dissect_map)
+    
     return proto_dissect_map
 
 
@@ -26,33 +30,40 @@ def custom_encoder(obj):
     return obj
 
 
+# Read json file
 def read_json_file(file_path):
     with open(file_path, 'r') as file:
         list = json.load(file)
     return list
 
 
+# Write to json file
 def write_to_map(fiel_path, proto_dissect_map):
     with open(fiel_path, 'w', encoding='utf-8') as file:
         json.dump(proto_dissect_map, file, indent=4, default = custom_encoder)
         print("Successfully written to file:proto_dissect_map.json")
 
 
-def select_proto(proto_dissect_map):
+# Select protocol by accessing proto table and dissect table
+def select(proto_dissect_map):
     proto_name = ''
     dissect_name = ''
     protos = []
     for map in proto_dissect_map:
-        protos.append(map['proto_name'])
+        protos.append(map['short_name'])
 
 
     for i, proto in enumerate(protos, 1):
         print(f"{i}. {proto}")
-    print("Modbus/TCP Protocol Number:980")
-    print("Modbus/UDP Protocol Number:981")
-    print("Modbus RTU Protocol Number:982")
-    print("Modbus Protocol Number:983")
-    print("S7Comm Protocol Number:1420")
+    print("----------------------Notes--------------------------")
+    print("DCCP Protocol Number: 316")
+    print("DNP 3.0 Protocol Number: 394")
+    print("Modbus/TCP Protocol Number: 980")
+    print("Modbus RTU Protocol Number: 982")
+    print("COTP Protocol Number: 1187")
+    print("S7Comm Protocol Number: 1420")
+    print("TPKT Protocol Number: 1591")
+    print("-----------------------------------------------------")
     proto_choice = int(input("Please enter the number:"))
 
     
@@ -60,7 +71,7 @@ def select_proto(proto_dissect_map):
         selected_proto = protos[proto_choice - 1]
         proto_name = selected_proto
         for map in proto_dissect_map:
-            if map['proto_name'] == proto_name:
+            if map['short_name'] == proto_name:
                 dissectors = map['dissect_name']
                 break
         for i, dissector in enumerate(dissectors, 1):
@@ -77,24 +88,30 @@ def select_proto(proto_dissect_map):
     return proto_name, dissect_name
 
 
-def main():
-        current_path = os.path.dirname(os.path.abspath(__file__))
-        pre_process_dir = os.path.join(current_path,'pre_process')
-        proto_list_path = os.path.join(pre_process_dir, "proto_list.json")
-        dissect_list_path = os.path.join(pre_process_dir, "dissect_list.json")
-        proto_list = read_json_file(proto_list_path)
-        dissect_list = read_json_file(dissect_list_path)
+# Select protocol name and dissect name
+def select_protocol():
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    pre_process_dir = os.path.join(current_path,'pre_process')
+    proto_list_path = os.path.join(pre_process_dir, "proto_table.json")
+    dissect_table_path = os.path.join(pre_process_dir, "dissect_table.json")
 
-        proto_dissect_map = convert_to_map(proto_list, dissect_list)
-        map_file_path = os.path.join(pre_process_dir, "proto_dissect_map.json")
-        write_to_map(map_file_path, proto_dissect_map)
+    # Read proto table and dissect table
+    proto_table = read_json_file(proto_list_path)
+    dissect_table = read_json_file(dissect_table_path)
 
-        proto_name, dissect_name = select_proto(proto_dissect_map)
-        print('---------------------------------------')
-        print('Selected Protocol:', proto_name)
-        print('Selected Dissector:', dissect_name)
-        print('---------------------------------------')
+    # Write associated protocols and parsers to a new json file
+    map_file_path = os.path.join(pre_process_dir, "proto_dissect_map.json")
+    proto_dissect_map = convert_to_map(proto_table, dissect_table, map_file_path)
 
+
+    # Select protocol by accessing proto table and dissect table
+    proto_name, dissect_name = select(proto_dissect_map)
+    print('---------------------------------------')
+    print('Selected Protocol:', proto_name)
+    print('Selected Dissector:', dissect_name)
+    print('---------------------------------------')
+
+    return proto_name, dissect_name
         
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
