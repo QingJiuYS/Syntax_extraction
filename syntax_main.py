13506f7a9946083dbe8ec95a33693a8235aefb03
QingJ <80376515+QingJiuYS@users.noreply.github.com>
2024-03-10 23:33:48 +0800

    Update syntax_main.py

--- syntax_main.py
+++ syntax_main.py
@@ -10,32 +10,37 @@ from main_parsing import main_parsing
 
 sys_dir = os.path.dirname(os.path.abspath(__file__))
 
+
 def main(args):
     # Two step: Pre_processing and Main_parsing
     parser = argparse.ArgumentParser(description="Command-line argument parser")
-    parser.add_argument('--pre_process', '-p')
-    # argp.add_argument('--select_protocol', '-s',action='store_true')
+
+    # Pre_processing, dest is the directory of Wireshark protocol dissector files
+    parser.add_argument('--pre_process', '-p', type=str, default=None, help="Input the directory path for pre-processing")
+    # Main_parsing
     parser.add_argument('--main_parsing', '-m')
     args = vars(parser.parse_args(args[1:]))
 
     time_start = time.time()
 
-    # Check pre_process directory exists
-    pre_processing_dir = os.path.abspath(os.path.join(sys_dir, 'pre_processing'))
-    if not os.path.exists(pre_processing_dir):
-        os.mkdir(pre_processing_dir) 
-        
-    # Check main_parsing directory exists
-    main_parsing_dir = os.path.abspath(os.path.join(sys_dir, 'main_parsing'))
-    if not os.path.exists(main_parsing_dir):
-        os.mkdir(main_parsing_dir) 
-
     # Pre_processing
     if args['pre_process']:
-        pre_process()
+        source_file_dir = args['pre_process']
+
+        # Check pre_process directory exists
+        pre_processing_dir = os.path.abspath(os.path.join(sys_dir, 'pre_processing'))
+        if not os.path.exists(pre_processing_dir):
+            os.mkdir(pre_processing_dir) 
+
+        pre_process(source_file_dir)
 
     # Main_parsing
     if args['main_parsing']:
+        # Check main_parsing directory exists
+        main_parsing_dir = os.path.abspath(os.path.join(sys_dir, 'main_parsing'))
+        if not os.path.exists(main_parsing_dir):
+            os.mkdir(main_parsing_dir)  
+
         # Select protocol 
         proto_name, dissect_name = select_protocol()    
 
